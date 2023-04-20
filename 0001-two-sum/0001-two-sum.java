7d625a3267badd48415c4960a13f523559324f10
Shivyanshi Shukla <80300678+shivyanshi@users.noreply.github.com>
2023-04-20 20:04:17 +0530

    Time: 71 ms (34.16%), Space: 43.2 MB (16.17%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+       int x[]= new int[2];
+        for(int i=0; i< nums.length;i++){
+            for(int j=i+1; j< nums.length; j++){
+                if(nums[i]+nums[j]==target){
+                    x[0]=i;
+                    x[1]=j;
+                    return x;
+                }
+            }
+        }
+        return x;
+    }
+}
